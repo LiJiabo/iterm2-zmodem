558bb62a0a744eeb4a949b50c8441aa96b78d474
李嘉博 <1658446745@qq.com>
2023-06-04 10:28:54 +0800

    汉化及修正路径
    汉化了提示信息，修正了rz和sz的路径

--- iterm2-recv-zmodem.sh
+++ iterm2-recv-zmodem.sh
@@ -3,23 +3,23 @@
 
 osascript -e 'tell application "iTerm2" to version' > /dev/null 2>&1 && NAME=iTerm2 || NAME=iTerm
 if [[ $NAME = "iTerm" ]]; then
-	FILE=$(osascript -e 'tell application "iTerm" to activate' -e 'tell application "iTerm" to set thefile to choose folder with prompt "Choose a folder to place received files in"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")")
+	FILE=$(osascript -e 'tell application "iTerm" to activate' -e 'tell application "iTerm" to set thefile to choose folder with prompt "选择要保存接收文件的目录"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")")
 else
-	FILE=$(osascript -e 'tell application "iTerm2" to activate' -e 'tell application "iTerm2" to set thefile to choose folder with prompt "Choose a folder to place received files in"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")")
+	FILE=$(osascript -e 'tell application "iTerm2" to activate' -e 'tell application "iTerm2" to set thefile to choose folder with prompt "选择要保存接收文件的目录"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")")
 fi
 
 if [[ $FILE = "" ]]; then
-	echo Cancelled.
+	echo 已取消
 	# Send ZModem cancel
 	echo -e \\x18\\x18\\x18\\x18\\x18
 	sleep 1
 	echo
-	echo \# Cancelled transfer
+	echo \# 已取消传输
 else
 	cd "$FILE"
-	/usr/local/bin/rz -E -e -b --bufsize 4096
+	/opt/homebrew/bin/rz -E -e -b --bufsize 4096
 	sleep 1
 	echo
 	echo
-	echo \# Sent \-\> $FILE
+	echo \# 已发送 \-\> $FILE
 fi
--- iterm2-send-zmodem.sh
+++ iterm2-send-zmodem.sh
@@ -3,20 +3,20 @@
 
 osascript -e 'tell application "iTerm2" to version' > /dev/null 2>&1 && NAME=iTerm2 || NAME=iTerm
 if [[ $NAME = "iTerm" ]]; then
-	FILE=`osascript -e 'tell application "iTerm" to activate' -e 'tell application "iTerm" to set thefile to choose file with prompt "Choose a file to send"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")"`
+	FILE=`osascript -e 'tell application "iTerm" to activate' -e 'tell application "iTerm" to set thefile to choose file with prompt "选择要保存接收文件的目录"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")"`
 else
-	FILE=`osascript -e 'tell application "iTerm2" to activate' -e 'tell application "iTerm2" to set thefile to choose file with prompt "Choose a file to send"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")"`
+	FILE=`osascript -e 'tell application "iTerm2" to activate' -e 'tell application "iTerm2" to set thefile to choose file with prompt "选择要保存接收文件的目录"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")"`
 fi
 if [[ $FILE = "" ]]; then
-	echo Cancelled.
+	echo 已取消
 	# Send ZModem cancel
 	echo -e \\x18\\x18\\x18\\x18\\x18
 	sleep 1
 	echo
-	echo \# Cancelled transfer
+	echo \# 已取消传输
 else
-	/usr/local/bin/sz "$FILE" --escape --binary --bufsize 4096
+	/opt/homebrew/bin/sz "$FILE" --escape --binary --bufsize 4096
 	sleep 1
 	echo
-	echo \# Received $FILE
+	echo \# 已接收 $FILE
 fi
